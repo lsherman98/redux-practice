b337511633c9ed481d5c3d101632549054662c75
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-03-30 15:00:33 -0400

    first commit

---
+++ src/cart-items.js
@@ -0,0 +1,28 @@
+const cartItems = [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img:
+      "https://www.smartcellular.us/media/catalog/product/cache/bf0ca523f7526fad50b11dad4e4bee43/s/a/samsung_s7_edge_gold_unlocked_1_2.jpg",
+    amount: 1
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img:
+      "https://media.wired.com/photos/5dae0207c96358000859e5a9/1:1/w_903,h_903,c_limit/Gear-Google-Pixel-4-Front-and-Back-SOURCE-Google.jpg",
+    amount: 1
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img:
+      "https://guide-images.cdn.ifixit.com/igi/RP3RqUqT2jQ3HRZP.medium",
+    amount: 1
+  }
+];
+
+export default cartItems;
